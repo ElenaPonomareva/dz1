4494524e7d14cd39415bc577fa40096ddcfa48ce
Elena <elenasurgut@mail.ru>
2022-04-13 21:30:30 +0300

    first

---
+++ DB.js
@@ -0,0 +1,23 @@
+const db = {
+  posts: [
+    {
+      name: 'Вера',
+      text: 'Кто любит бога, добра получит много.',
+      photo: 'https://spravochnick.ru/assets/files/handbook/images/16/82/1682a87f5ec187e684ed8b5f5a8eaff8',
+    },
+    {
+      name: 'Надежда',
+      text: 'Где жизнь, там и надежда.',
+      photo: 'https://pravlife.org/sites/default/files/field/image/9783c03fa7206cc.jpg',
+    },
+    {
+      name: 'Любовь',
+      text: 'Где любовь да совет, там и горя нет.',
+      photo: 'https://img5.goodfon.ru/wallpaper/nbig/8/20/liubov-serdtse-red-love-brelok-heart-romantic-i-love-you-key.jpg',
+    },
+  ],
+}
+
+module.exports = {
+  db,
+}
